f2924985724622d40af5881679a4746170e54f53
ajayrajput11 <ajayrajput171204@gmail.com>
2025-03-28 17:12:15 +0530

    ajaysingh/left-comp-todo-complete

--- src/components/LeftComponent.tsx
+++ src/components/LeftComponent.tsx
@@ -1,9 +1,26 @@
-
+import { LeftData } from "../constent";
+import "./LeftComonent.css"
 
 function LeftComponent() {
   return (
-    <div className="card-box">
-      Left Component
+    <div className="card-box lComp">
+      <p>Todo</p>
+      
+      {
+     LeftData.map((data)=>{
+      return (
+        <div className="title ">
+          <div className="d-flex justify-content-between"><h5>{data.title}</h5>
+            <p><span><i className="fa-solid fa-pen-to-square"></i></span> &ensp; <span><i className="fa-solid fa-trash"></i></span></p>
+            </div>
+
+         <div className="d-flex justify-content-between">
+         <p>{data.details}</p> 
+         <p>{data.discription}</p>
+         </div>
+         </div>
+      )
+     })}
     </div>
   )
 }
---
+++ src/constent.ts
@@ -0,0 +1,5 @@
+const LeftData = [{title:"Memorise a code",discription:"completed",details:"do it"},
+    {title:"Lunch",discription:"incomplete",details:"12:30"},
+    {title:"Groceries",discription:"incomplete",details:"list of itenms"}
+];
+ export {LeftData}
