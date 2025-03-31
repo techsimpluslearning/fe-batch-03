e95ccc338a2f17deb36a943ee9cd7f838d7bd587
shivani lodhi <shivanilodhilodhi856@ gmail.com>
2025-03-31 16:23:37 +0530

    updated changes

--- src/components/Addnewtaskbutton.tsx
+++ src/components/Addnewtaskbutton.tsx
@@ -24,9 +24,47 @@
 
 // export default Addnewtaskbutton;
 
+// import { useState } from "react";
+// import { Button, Placeholder } from "reactstrap";
+// import { Offcanvas, OffcanvasHeader, OffcanvasBody } from "reactstrap";
+
+// const Addnewtaskbutton = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const onclicksidebar = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <div>
+//       <Offcanvas direction="end" isOpen={isOpen} toggle={onclicksidebar}>
+//         <OffcanvasHeader toggle={onclicksidebar} className="offcanvasheader">
+//           Add New Task
+//         </OffcanvasHeader>
+//         <OffcanvasBody>
+//           <input type="text" placeholder="Title" className="searchbar" />
+//           <input type="text" placeholder="Description" className="searchbar" />
+
+//           <Button className="btnbackground" onClick={onclicksidebar}>
+//             Create new task
+//           </Button>
+//         </OffcanvasBody>
+//       </Offcanvas>
+//       <div>
+//         <Button className="btnbackground" onClick={onclicksidebar}>
+//           Create new task
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Addnewtaskbutton;
+
+
+
 import { useState } from "react";
-import { Button, Placeholder } from "reactstrap";
-import { Offcanvas, OffcanvasHeader, OffcanvasBody } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
 
 const Addnewtaskbutton = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -35,24 +73,70 @@ const Addnewtaskbutton = () => {
     setIsOpen(!isOpen);
   };
 
+  const buttonText = "Add New Task";
+
   return (
     <div>
-      <Offcanvas direction="end" isOpen={isOpen} toggle={onclicksidebar}>
-        <OffcanvasHeader toggle={onclicksidebar} className="offcanvasheader">
-          Add New Task
-        </OffcanvasHeader>
-        <OffcanvasBody>
-          <input type="text" placeholder="Title" className="searchbar" />
-          <input type="text" placeholder="Description" className="searchbar" />
+      <Modal 
+        className="model"
+        isOpen={isOpen} 
+        toggle={onclicksidebar} 
+        centered // This makes the modal open in the center
+      >
+        <ModalHeader toggle={onclicksidebar} className="modalheader">
+          {buttonText}
+        </ModalHeader>
+        <ModalBody>
+          <input 
+            type="text" 
+            placeholder="Title" 
+            className="searchbar mb-2 p-2" 
+          />
+          <textarea  
+           type="text" 
+            placeholder="Description" 
+            className="searchbar mb-3 p-2" />
 
-          <Button className="btnbackground" onClick={onclicksidebar}>
-            Create new task
+
+         <select className="searchbar mb-3 p-2">
+            <option value="" disabled selected hidden>
+               Status
+            </option>
+            <option value="progress">Progress</option>
+            <option value="Pending">Pending</option>
+            <option value="Complete">Complete</option>
+          </select>
+
+            <input 
+            type="date" 
+            placeholder="Due Date" 
+            className="searchbar mb-3 p-2" 
+          />
+            <select className="searchbar mb-3 p-2">
+            <option value="" disabled selected hidden>
+               Priority
+            </option>
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+          </select>
+
+          <Button
+            className="btnbackground" 
+            onClick={onclicksidebar}
+          >
+           {buttonText}
           </Button>
-        </OffcanvasBody>
-      </Offcanvas>
+        </ModalBody>
+      </Modal>
+
       <div>
-        <Button className="btnbackground" onClick={onclicksidebar}>
-          Create new task
+        <Button 
+          color="" 
+          className="btnbackground" 
+          onClick={onclicksidebar}
+        >
+          {buttonText}
         </Button>
       </div>
     </div>
@@ -60,3 +144,4 @@ const Addnewtaskbutton = () => {
 };
 
 export default Addnewtaskbutton;
+
